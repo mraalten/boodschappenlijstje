a800aadce537f69305b55079e41425c05611381a
Aalten <richard.aalten@rabobank.nl>
2019-02-01 18:52:48 +0100

    Product toevoegen button added

---
+++ src/client/src/app/product-muteren/product-muteren.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'product-muteren',
+  templateUrl: './product-muteren.component.html',
+  styleUrls: ['./product-muteren.component.css']
+})
+export class ProductMuterenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
